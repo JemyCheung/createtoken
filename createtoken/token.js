283a0bde54c2056ff4fa5a416c2dde6c6f63e97a
JemyCheung <37655628+JemyCheung@users.noreply.github.com>
2022-11-08 18:21:24 +0800

    Delete token.js

--- createtoken/token.js
+++
@@ -1,7 +0,0 @@
-var qiniu = require('qiniu')
-var mac = new qiniu.auth.digest.Mac("MP_Ebql_lSsUrDr7WrXn_5vKocQDLvTPCNE", "YZlfOKeuQVA0h7yuCJrkdcYlbcGYwEP7A8YVG9-P");
-//url schema + "://" + host + path
-var qntoken = qiniu.util.generateAccessTokenV2(mac, "url" , "method", "contentType", "body");
-console.log(qntoken);
-
-var acctoken = qiniu.util.generateAccessToken(mac, "url", "contentType", "body");
